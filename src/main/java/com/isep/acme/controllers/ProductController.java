8bff9273f84a603e13a0df093ab67b323938e7d0
Ana Paula Rabelo <prabelo79@gmail.com>
2023-04-03 20:35:47 +0100

    chore: change eureka client configuration
    create new yml file for eureka configuration and format code identation

--- src/main/java/com/isep/acme/controllers/ProductController.java
+++ src/main/java/com/isep/acme/controllers/ProductController.java
@@ -1,41 +1,47 @@
 package com.isep.acme.controllers;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.isep.acme.model.Product;
 import com.isep.acme.model.ProductDTO;
-
 import com.isep.acme.services.ProductService;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Optional;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 
 @Tag(name = "Product", description = "Endpoints for managing  products")
 @RestController
 @RequestMapping("/products")
+@Slf4j
 class ProductController {
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
     private ProductService service;
 
-
     @Operation(summary = "gets catalog, i.e. all products")
     @GetMapping
     public ResponseEntity<Iterable<ProductDTO>> getCatalog() {
-       final var products = service.getCatalog();
-
-       return ResponseEntity.ok().body( products );
+        final var products = service.getCatalog();
+        log.info("Obtaining status...");
+        return ResponseEntity.ok().body(products);
     }
 
     @Operation(summary = "finds product by sku")
@@ -44,19 +50,20 @@ class ProductController {
 
         final Optional<ProductDTO> product = service.findBySku(sku);
 
-        if( product.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Product not found.");
+        if (product.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found.");
         else
             return ResponseEntity.ok().body(product.get());
     }
 
     @Operation(summary = "finds product by designation")
     @GetMapping(value = "/designation/{designation}")
-    public ResponseEntity<Iterable<ProductDTO>> findAllByDesignation(@PathVariable("designation") final String designation){
+    public ResponseEntity<Iterable<ProductDTO>> findAllByDesignation(
+            @PathVariable("designation") final String designation) {
 
-        final Iterable<ProductDTO> products = service.findByDesignation( designation );
-        
-        return ResponseEntity.ok().body( products );
+        final Iterable<ProductDTO> products = service.findByDesignation(designation);
+
+        return ResponseEntity.ok().body(products);
     }
 
     @Operation(summary = "creates a product")
@@ -66,27 +73,27 @@ class ProductController {
         try {
             final ProductDTO product = service.create(manager);
             return new ResponseEntity<ProductDTO>(product, HttpStatus.CREATED);
-        }
-        catch( Exception e ) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT,"Product must have a unique SKU.");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Product must have a unique SKU.");
         }
     }
 
     @Operation(summary = "updates a product")
     @PatchMapping(value = "/{sku}")
-    public ResponseEntity<ProductDTO> Update(@PathVariable("sku") final String sku, @RequestBody final Product product) {
+    public ResponseEntity<ProductDTO> Update(@PathVariable("sku") final String sku,
+            @RequestBody final Product product) {
 
         final ProductDTO productDTO = service.updateBySku(sku, product);
 
-        if( productDTO == null )
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Product not found.");
+        if (productDTO == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found.");
         else
             return ResponseEntity.ok().body(productDTO);
     }
 
     @Operation(summary = "deletes a product")
     @DeleteMapping(value = "/{sku}")
-    public ResponseEntity<Product> delete(@PathVariable("sku") final String sku ){
+    public ResponseEntity<Product> delete(@PathVariable("sku") final String sku) {
 
         service.deleteBySku(sku);
         return ResponseEntity.noContent().build();
